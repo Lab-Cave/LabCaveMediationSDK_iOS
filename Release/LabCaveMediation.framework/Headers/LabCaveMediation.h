b465f7d596af7968ed9c55f1a235bbd6c2df7197
Jaime López <brezoso@gmail.com>
2019-07-01 10:07:19 +0200

    Version 2.8.1

--- Release/LabCaveMediation.framework/Headers/LabCaveMediation.h
+++ Release/LabCaveMediation.framework/Headers/LabCaveMediation.h
@@ -5,7 +5,7 @@
 //  Created by LabCaveGames on 21/2/17.
 //  Copyright © 2017 Lab Cave Apps S.L. All rights reserved.
 //
-// LabCaveGames SDK 2.8.0
+// LabCaveGames SDK 2.8.1
 
 #import <Foundation/Foundation.h>
 #import <CoreLocation/CoreLocation.h>
